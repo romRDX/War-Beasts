26bd3365e3e932917d46e1510c0d50d976ce45fb
Rômullo Sander <romullo_sas@outlook.com>
2022-11-02 19:06:40 -0300

    start battle 1

---
+++ web/src/telas/TelaBatalha/Ambiente/context/BattleContext.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const BattleContext = React.createContext();
+
+export default BattleContext;
--- web/src/telas/TelaBatalha/Ambiente/index.js
+++ web/src/telas/TelaBatalha/Ambiente/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Container } from './styles';
 
@@ -6,14 +6,49 @@ import PlayerCard from './components/PlayerCard';
 import PlayerModel from './components/PlayerModel';
 import EnemyModel from './components/EnemyModel';
 import BottomMenu from './components/BottomMenu';
+import { apiWB } from 'services/axios';
+import { useCharacter } from 'hooks/useCharacter';
+import { useAuth } from 'hooks/useAuth';
+import { useMap } from 'hooks/useMap';
+import BattleContext from './context/BattleContext';
 
 const Ambiente = () => {
+
+    const { selectedCharacter } = useCharacter();
+    const { authData } = useAuth();
+    const { activeStage } = useMap();
+
+    const [battleId, setBattleId] = useState(null);
+    const [monsterData, setMonsterData] = useState(null);
+
+    useEffect(() => {
+
+        if(authData && activeStage && selectedCharacter){
+            console.log("FOI");
+            apiWB.post("/battle-start", {
+                params: JSON.stringify({
+                        playerId: authData.id,
+                        characterId: selectedCharacter.id,
+                        stageId: activeStage.id
+                    })
+                }
+            ).then((resp) => {
+                if(resp.data.success){
+                    setBattleId(resp.data.battleId);
+                    setMonsterData(resp.data.monsterData);
+                }
+            })
+        }
+    }, [authData, selectedCharacter, activeStage]);
+
     return (
         <Container>
-            <PlayerCard />
-            <PlayerModel />
-            <EnemyModel />
-            <BottomMenu />
+            <BattleContext.Provider value={{ battleId, monsterData }}>
+                <PlayerCard />
+                <PlayerModel />
+                <EnemyModel />
+                <BottomMenu />
+            </BattleContext.Provider>
         </Container>
     )
 }
